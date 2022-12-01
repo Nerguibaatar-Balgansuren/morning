2d2c61d1d3dd49daab0dcc8f4acf22e83ed6f98d
Nerguibaatar-Balgansuren <morning@admins-iMac.local>
2022-12-01 12:30:24 +0800

    added js loop

---
+++ JS Loop/1.js
@@ -0,0 +1,12 @@
+count = 0 ;
+
+result = "" ;
+
+while(count < 100) {
+    if (count % 2 == 1) {result = result + " " + count
+ } ;
+
+    count++
+} ;
+
+console.log(result);
--- JS String/Огноо хэвлэх, Оператор олох/1.js
+++ JS String/Огноо хэвлэх, Оператор олох/1.js
@@ -1,28 +1,40 @@
  year = prompt ("жил оруулна уу?")
+ 
+
  month = prompt ("сар оруулна уу?")
  day = prompt ("өдөр оруулна уу?")
 
+yearNumber = Number(year);
+monthNumber = Number(month);
+dayNumber = Number(day);
 
-
-
- month.length == 1 ? month = '0' + month : month = month;
+month.length == 1 ? month = '0' + month : month = month;
 
 day.length == 1 ? day = '0' + day : day = day;
 
 var show = year + '-' + month + '-' + day
 
-if ( month <= 12 == 0) {
+if (!Number.isInteger(yearNumber)) { 
+alert("Ta jilee zov oruulna uu?")
+    show = "Ta jilee zov oruulna uu?"
+}   
+ console.log(!monthNumber > 0);
+console.log(!monthNumber < 12);
 
-} else {
-    show = "month is wrong"
-    console.error("month is not right");
+// 24 < 12 -> !false
+// 2 < 12 -> true false
+
+
+
+
+if (!Number.isInteger(monthNumber) || monthNumber < 0 || monthNumber > 12) {
+
+    
+    alert("Ta saraa zov oruulna uu?")
+    show = "Ta saraa zov oruulna uu?" 
 }
 
-if (day <= 31 == 0) {
-
-} else {
-    show = "day is wrong"
-    console.error(
-        "day is wrong"
-    );
+if (!Number.isInteger(dayNumber) || dayNumber < 0 || dayNumber > 31) {
+    alert("Ta odrooo zov oruulna uu?")
+    show = "Ta odroo zov oruulna uu?" 
 }
