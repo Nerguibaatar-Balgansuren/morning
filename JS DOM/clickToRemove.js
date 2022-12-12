9fb43d89e4a42ac2b49cca9f85b2a600509988dc
Nerguibaatar-Balgansuren <morning@admins-iMac.local>
2022-12-12 12:31:46 +0800

    added js dom

---
+++ JS DOM/clickToRemove.js
@@ -0,0 +1,34 @@
+
+
+selected = document.getElementById('colorSelect');
+selectedId = document.getElementById('colorSelect');
+body = document.getElementsByTagName("body")[0];
+optionId = document.getElementsByTagName("option");
+
+
+// document.removeChild()
+
+console.log(selected);
+console.log(body);
+
+doButton = document.getElementsByTagName('input')[0];
+
+console.log(doButton);
+
+// function remover(optionId) {
+//     selected.removeAttribute(option)
+// }
+
+// function bGChanger(params) {
+//     body.style.backgrounColor = "green" 
+// }
+
+function removeSelf(params) {
+    nodeToRemove.selected.removeChild(nodeToRemove)
+}
+
+doButton.addEventListener("click", ()=>{body.style = "background-color: green"});
+
+doButton.addEventListener("click", ()=>{.selected.})
+
+console.log(doButton);
