825ef1ab5dd978787c4dc1c04170a1e596a146a3
Nerguibaatar-Balgansuren <morning@admins-iMac.local>
2022-12-19 12:34:17 +0800

    todo2

---
+++ JS DOM/aimgiinJagsaalt.js
@@ -0,0 +1,13 @@
+aimagList = document.getElementById("aimagNames");
+
+console.log(aimagList);
+
+aimagOptionList = document.getElementById("aimagOptionList");
+
+aimagOptionList = document.createElement("option")
+
+aimag1 = "aimag1"
+aimagOptionList.appendChild("aimag1");
+
+aimagOptionList.setAttribute("val", "value1")
+
---
+++ To do list/toDo2.js
@@ -0,0 +1,42 @@
+main = document.getElementsByTagName("main")[0];
+main.innerHTML = `<div class="section1">
+<h1>Todo List</h1>
+<div class="ognooRow"></div>
+<form action="">
+  <input type="text" placeholder="Таскаа оруулна уу?" />
+  <button>+Нэмэх</button>
+</form>
+</div>
+<div class="section2">
+<h1>Жагсаалт</h1>
+<div class="list">
+    <div class="lItem">
+        <div class="lName"></div>
+        <div class="lOptions"></div>
+    </div>
+</div>
+</div>`;
+
+list = []
+
+button = document.getElementsByTagName("button")[0];
+button.addEventListener("click", add);
+function add(event) {
+
+
+    event.preventDefault();
+ 
+    localStorage.setItem()
+
+
+ 
+ 
+ //   let  lItem = document.createElement("div");
+ //     lItem.classList.add("lItem", "flex");
+     
+ //     lItem.innerHTML = input.value;
+ //     lItem.id = "1"
+ 
+ //     list.appendChild(lItem)
+     
+ }
